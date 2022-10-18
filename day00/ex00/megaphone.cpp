a54f0ae3d85fe45a074e4afa727221ac7228c9dc
Chery Pollitor <cpollito@at-f6.msk.21-school.ru>
2022-10-18 16:03:53 +0300

    ex00  Please enter the commit message for your changes. Lines starting

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpollito <cpollito@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 13:49:19 by cpollito          #+#    #+#             */
+/*   Updated: 2022/10/18 15:56:42 by cpollito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+	char	c;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i = 1;
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)(std::toupper(argv[i][j]));
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
